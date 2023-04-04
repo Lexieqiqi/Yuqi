7cb911a16cace5c05fc7bdfbef1ec78793e78588
Lexieqiqi <lexieqiqi@gmail.com>
2023-04-05 01:48:14 +0800

    force path

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div>
         <video className="fixed z-0 opacity-60 top-0 left-0 w-full h-full object-cover" autoPlay muted loop>
-            <source src='Yuqi/sunrise.mp4' type="video/mp4" /> 
+            <source src='/sunrise.mp4' type="video/mp4" /> 
         </video>
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.9 }}>
     <div className="fixed top-20 w-full h-full overflow-y-auto">
