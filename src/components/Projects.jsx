c8a07f8e6821e1d58003b2060fa27050e2c9fda4
Lexieqiqi <lexieqiqi@gmail.com>
2023-04-04 12:28:04 +0800

    test

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Image from 'next/image'
-import gallangImg from "public/projects/gallang.png"
-import furhatImg from "public/projects/furhat.png"
-import starlampImg from "public/projects/starlamp.png"
-import searchengineImg from "public/projects/searchengine.png"
-import Link from 'next/link'
 import ProjectItem from './ProjectItem'
+import Skills from '@component/Skills'
+import gallangImg from "@images/gallang.png"
+import furhatImg from "@images/furhat.png"
+import starlampImg from "@images/starlamp.png"
+import searchengineImg from "@images/searchengine.png"
 
 const Projects = () => {
   return (
     <div id='projects' className='w-full lg:h-screen'>
+        <Skills />
         <div className='max-w-[1240px] mx-auto px-2 py-16'>
             <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
             <h2 className='py-4'>What I've built</h2>
