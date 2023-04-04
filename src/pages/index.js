48f5cc9b0b3c079f298bf88caced32a30b073c70
Lexieqiqi <lexieqiqi@gmail.com>
2023-04-04 12:19:53 +0800

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import About from '@component/About'
 import Skills from '@component/Skills'
 import Projects from '@component/Projects'
 import Contact from '@component/Contact'
+import gallangImg from "@images/gallang.png"
 
 export default function Home() {
       
